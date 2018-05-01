c4080cbb0e1ef3ac4b173b1d9d693302c7497058
MathiasMehl <mmsa17@student.aau.dk>
2018-05-01 10:28:23 +0200

    changed datatypes of gui elements from java 10 to java 9

--- datchain/src/dk/aau/dat/a311b/datchain/gui/Search.java
+++ datchain/src/dk/aau/dat/a311b/datchain/gui/Search.java
@@ -9,20 +9,20 @@ import javafx.stage.Stage;
 
 public class Search {
     public static void Search() {
-        var Search_stage = new Stage();
+        Stage Search_stage = new Stage();
         Search_stage.setTitle("Search");
-        var root = new GridPane();
+        GridPane root = new GridPane();
         root.setVgap(7);
         root.setHgap(5);
         root.setPadding(new Insets(10, 10, 10, 10));
-        var scene = new Scene(root,600, 230);
+        Scene scene = new Scene(root,600, 230);
 
 
-        var term_label = new Label("Input:");
+        Label term_label = new Label("Input:");
         term_label.setMinWidth(150);
         root.setConstraints(term_label,0,0);
         root.getChildren().add(term_label);
-        var term_text = new TextField();
+        TextField term_text = new TextField();
         root.setConstraints(term_text,0,1);
         root.getChildren().add(term_text);
 
@@ -48,11 +48,11 @@ public class Search {
         root.setConstraints(ID_text,0,5);
         root.getChildren().add(ID_text);
         */
-        var Search_button = new Button("Search");
+        Button Search_button = new Button("Search");
         root.setConstraints(Search_button,0,7);
         root.getChildren().add(Search_button);
 
-        var listView = new ListView<>();
+        ListView listView = new ListView<>();
         listView.setMinWidth(420);
         listView.getItems().addAll("eks. 1", "eks. 2", "eks. 3");
         root.setConstraints(listView,1,0,1,7);
