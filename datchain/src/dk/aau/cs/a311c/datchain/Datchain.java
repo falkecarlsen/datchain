341de5cb2a6c85b8a8686803010a6c898ab99858
cogitantium <falke.cs@gmail.com>
2018-05-10 21:56:34 +0200

    added sanity check to main

--- datchain/src/dk/aau/cs/a311c/datchain/Datchain.java
+++ datchain/src/dk/aau/cs/a311c/datchain/Datchain.java
@@ -2,6 +2,9 @@ package dk.aau.cs.a311c.datchain;
 
 import dk.aau.cs.a311c.datchain.utility.RSA;
 
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class Datchain {
@@ -27,20 +30,22 @@ public class Datchain {
 
         System.out.println("chain02 validated: " + chain02.validateChain());
 
-        //testing out fuzzy search matching
-        //Block searchResultBlock01 = chain02.searchSingleIdentity("citiz nahym");
-        //Block searchResultBlock02 = chain02.searchSinglePublicKey("gænæsæs Påblæk kay");
-        //System.out.println(searchResultBlock01.getIdentity());
-        //System.out.println(searchResultBlock02.getIdentity() + " " + searchResultBlock02.getIdentityPublicKey());
 
-        System.out.println("RSA-keys present: " + RSA.keysPresent());
-        //Byte[] ciphertext = RSAgen.encrypt("Random String", pubkey);
-        //System.out.println("Decrypted text: " + RSAgen.decrypt(ciphertext, privkey));
+        System.out.println("RSA-keys present: " + RSA.keysPresent("data/"));
 
-        //create Search object and run identity search for printing
-        ArrayList<Block> temp = new Search().FuzzySearchIdentity("citizz", chain02, 2);
-        temp.forEach(block -> System.out.println(block.getIdentity()));
-        System.out.println("RSA keys generated: " + RSA.keyPairWriter());
-        System.out.println("RSA-keys present: " + RSA.keysPresent());
+        KeyPair keyPair = RSA.keyPairInit();
+
+        PublicKey publicKey = RSA.getPublicKey(keyPair);
+        PrivateKey privateKey = RSA.getPrivateKey(keyPair);
+
+        System.out.println("RSA keys generated: " + RSA.keyPairWriter(keyPair, "data/"));
+
+        PublicKey publicKeyFromFile = RSA.getPublicKeyFromFile("data/public.key");
+        PrivateKey privateKeyFromFile = RSA.getPrivateKeyFromFile("data/private.key");
+
+        System.out.println("Public keys from file equal? " + publicKey.equals(publicKeyFromFile));
+        System.out.println("Private keys from file equal? " + privateKey.equals(privateKeyFromFile));
+
+
     }
 }
