461cafb43c59efa4d708bc07eb0e8181c4d66d76
cogitantium <falke.cs@gmail.com>
2018-05-14 23:49:32 +0200

    refactored CipherBlock, optimising build and removing redundancies

--- datchain/src/dk/aau/cs/a311c/datchain/utility/CipherBlock.java
+++ datchain/src/dk/aau/cs/a311c/datchain/utility/CipherBlock.java
@@ -8,50 +8,43 @@ import static dk.aau.cs.a311c.datchain.utility.RSA.encrypt;
 
 public class CipherBlock {
 
-    //choosing 2^8 as blockSize for playing along with filesystem preferences
+    //choosing 2^8, but below modulo of RSA keys, as blockSize for playing along with filesystem preferences
     private static final int blockSize = 256;
-    private static int numBlocks = 0;
     final private String cleartext;
     private byte[][] cipherBlock;
     private byte[][] clearBlock;
     private String decryptedText;
 
     //TODO implement reverse operation (RSA signature)
+    //TODO by passing generic key, both encrypt and decrypt operations can be facilitated with one method
 
     public CipherBlock(String source) {
-
-        //assign source to cleartext
+        //assign source string to cleartext and build block
         this.cleartext = source;
+        buildBlock();
     }
 
-    public void buildBlock() {
-
+    private void buildBlock() {
         //get numBlocks from current input by utilising int division flooring and adding one
-        numBlocks = (this.cleartext.getBytes().length / blockSize) + 1;
+        int numBlocks = (this.cleartext.getBytes().length / blockSize) + 1;
 
-        //create dimensions of both blocks with calculated properties
+        //create dimensions of both two-dimensional byte arrays with calculated properties
         cipherBlock = new byte[numBlocks][blockSize];
         clearBlock = new byte[numBlocks][blockSize];
 
-        //assign temporary bytearray of cleartext in bytes
-        byte[] cleartextBytes = this.cleartext.getBytes();
         //create counter for keeping track of current block of text
         int startIndex = 0;
 
-        //for number of blocks needed to hold source, do
+        //for number of blocks needed to hold source in block, do
         for (int i = 0; i < numBlocks; i++) {
-            //copy bytes from startIndex, blockSize-bytes forward and pass bytes to cipherBlock
-            cipherBlock[i] = Arrays.copyOfRange(cleartextBytes, startIndex, startIndex + blockSize);
+            //copy cleartext bytes from startIndex, blockSize-bytes forward and pass bytes to cipherBlock
+            cipherBlock[i] = Arrays.copyOfRange(this.cleartext.getBytes(), startIndex, startIndex + blockSize);
             //move startIndex ahead blockLimit for next pass
             startIndex += blockSize;
         }
     }
 
-    //TODO below
-    //by passing generic key, both encrypt and decrypt operations can be facilitated with one method
-
     public void encryptBlock(Key key) {
-
         //declare counter to keep track of block currently iterating over
         int index = 0;
 
@@ -63,7 +56,6 @@ public class CipherBlock {
     }
 
     public void decryptBlock(Key key) {
-
         //declare counter to keep track of block currently iterating over
         int index = 0;
 
@@ -82,7 +74,6 @@ public class CipherBlock {
         for (byte[] block : this.clearBlock) {
             stringBuilder.append(new String(block));
         }
-
         //assign newly built and trimmed string to decryptedText
         this.decryptedText = stringBuilder.toString().trim();
     }
@@ -93,13 +84,5 @@ public class CipherBlock {
 
     public String getDecryptedText() {
         return this.decryptedText;
-    }
-
-    int getBlockSize() {
-        return blockSize;
-    }
-
-    int getNumBlocks() {
-        return numBlocks;
     }
 }
